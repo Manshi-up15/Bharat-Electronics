d54c2ff3b41d9f426f2599ee5862fe371555bde8
Manshi-up15 <sainimanshi93@gmail.com>
2026-06-18 15:25:24 +0530

    fixed the categories

--- app/(public)/categories/page.tsx
+++ app/(public)/categories/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { getCategories } from "../../../lib/models";
-
+export const dynamic = "force-dynamic";
 export default async function CategoriesPage() {
   const categories = await getCategories();
 
